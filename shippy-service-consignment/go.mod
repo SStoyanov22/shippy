cbfea77503c68754cb283c6e024d4e40d1fdf469
Stoyan Stoyanov <SStoyanov22@gmail.com>
2022-05-05 13:49:22 +0300

    updated

--- shippy-service-consignment/go.mod
+++ shippy-service-consignment/go.mod
@@ -3,10 +3,10 @@ module github.com/SStoyanov22/shippy/shippy-service-consignment
 go 1.18
 
 require (
+	github.com/SStoyanov22/shippy/shippy-service-user v0.0.0-20220505060350-b3c56de3d109
 	github.com/SStoyanov22/shippy/shippy-service-vessel v0.0.0-00010101000000-000000000000
-	github.com/pkg/errors v0.9.1
+	go-micro.dev/v4 v4.6.0
 	go.mongodb.org/mongo-driver v1.9.0
-	google.golang.org/grpc v1.45.0
 	google.golang.org/protobuf v1.28.0
 )
 
@@ -34,6 +34,7 @@ require (
 	github.com/nxadm/tail v1.4.8 // indirect
 	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
 	github.com/sergi/go-diff v1.1.0 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
@@ -43,14 +44,12 @@ require (
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	go-micro.dev/v4 v4.6.0 // indirect
-	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f // indirect
 	golang.org/x/net v0.0.0-20220418201149-a630d4f3e7a2 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	google.golang.org/genproto v0.0.0-20220414192740-2d67ff6cf2b4 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
--- shippy-service-consignment/main.go
+++ shippy-service-consignment/main.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 
 	pb "github.com/SStoyanov22/shippy/shippy-service-consignment/proto/consignment"
-	vesselProto "github.com/SStoyanov22/shippy/shippy-service-vessel/proto/vessel"
+	userService "github.com/SStoyanov22/shippy/shippy-service-user/proto/user"
+	vesselService "github.com/SStoyanov22/shippy/shippy-service-vessel/proto/vessel"
 	"go-micro.dev/v4"
+	"go-micro.dev/v4/client"
+	"go-micro.dev/v4/metadata"
+	"go-micro.dev/v4/server"
 )
 
 func main() {
 	// Set-up micro instance
 	service := micro.NewService(
 		micro.Name("shippy-service-consignment"),
+		micro.Version("latest"),
+		micro.WrapHandler(AuthWrapper),
 	)
 
 	service.Init()
@@ -30,7 +37,7 @@ func main() {
 	consignmentCollection := client.Database("shippy").Collection("consignments")
 
 	repository := &MongoRepository{consignmentCollection}
-	vesselClient := vesselProto.NewVesselService("shippy-service-vessel", service.Client())
+	vesselClient := vesselService.NewVesselService("shippy-service-vessel", service.Client())
 	h := &handler{repository, vesselClient}
 
 	// Register handlers
@@ -39,5 +46,35 @@ func main() {
 	// Run the server
 	if err := service.Run(); err != nil {
 		fmt.Println(err)
+	}
+}
+
+// AuthWrapper is a high-order function which takes a HandlerFunc
+// and returns a function, which takes a context, request and response interface.
+// The token is extracted from the context set in our consignment-cli, that
+// token is then sent over to the user service to be validated.
+// If valid, the call is passed along to the handler. If not,
+// an error is returned.
+func AuthWrapper(fn server.HandlerFunc) server.HandlerFunc {
+	return func(ctx context.Context, req server.Request, resp interface{}) error {
+		meta, ok := metadata.FromContext(ctx)
+		if !ok {
+			return errors.New("no auth meta-data found in request")
+		}
+
+		// Note this is now uppercase (not entirely sure why this is...)
+		token := meta["Token"]
+		log.Println("Authenticating with token: ", token)
+
+		// Auth here
+		authClient := userService.NewUserServiceClient("shippy-service-user", client.DefaultClient)
+		_, err := authClient.ValidateToken(context.Background(), &userService.Token{
+			Token: token,
+		})
+		if err != nil {
+			return err
+		}
+		err = fn(ctx, req, resp)
+		return err
 	}
 }
